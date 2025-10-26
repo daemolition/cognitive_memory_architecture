71dd78c7ca01df624d4a49eea944214fe6a00fa6
Daemolition <crizz-t@gmx.de>
2025-10-26 17:56:53 +0100

    Changed to postgres

--- src/components/sql.py
+++ src/components/sql.py
@@ -1,15 +1,20 @@
 import logging
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker, scoped_session
+from ..models.persistant_memory_model import Base
 
 class SQL:
     def __init__(self, filename: str="stmemory.sqlite3"):
         self.filename = filename
-        self.sqlite_uri = f"sqlite:///{self.filename}"
-        self.connection_args = {"check_same_thread": False}
+        self.database_uri = f"postgresql+psycopg2://postgres:postgres@localhost:5432/lstm_rag"
         self.engine = self.get_engine()
-        self.SessionLocal = sessionmaker(bind=self.engine, autoflush=False, autocommit=False)
+        self.SessionLocal = scoped_session(sessionmaker(bind=self.engine, autoflush=False, autocommit=False))
+        
+        self.metadata = Base.metadata
+        
+    def create_all(self):
+        Base.metadata.create_all(bind=self.engine)
         
     def get_db(self):
         db: Session = self.SessionLocal()
@@ -21,4 +26,4 @@ class SQL:
             db.close()
             
     def get_engine(self):
-        return create_engine(self.sqlite_uri, connect_args=self.connection_args)
+        return create_engine(self.database_uri)
