4410e277d66637305af87cfbb6ca48f31d03140c
Daemolition <crizz-t@gmx.de>
2025-10-29 22:21:41 +0100

    Added a function to count tokens

--- src/models/model_base.py
+++ src/models/model_base.py
@@ -1,10 +1,12 @@
 from langchain_ollama import ChatOllama
+from transformers import AutoTokenizer
 
 class LLM:
-    def __init__(self, model: str ="gemma3:4b-it-q4_K_M", context: int = 32768, reasoning: bool = False):
+    def __init__(self, model: str ="google/gemma-3-4b-it", context: int = 32768, reasoning: bool = False):
         self.model = model
         self.context = context
         self.reasoning = reasoning
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
     
     def load_llm(self):
         """Initializes the llm"""        
@@ -14,3 +16,7 @@ class LLM:
             reasoning=self.reasoning
         )
         return llm
+    
+    def count_tokens(self, text: str) -> int:
+        """Returns the correct number of tokens"""
+        return len(self.tokenizer.encode(text, add_special_tokens=False))
